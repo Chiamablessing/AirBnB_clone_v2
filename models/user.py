279ad60848677d7f5452137943465534866be494
Chiamablessing <chiamablessing@gmail.com>
2024-04-17 07:43:30 +0100

    Dbstorage user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python3
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
-
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
@@ -9,4 +14,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    
+     places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
