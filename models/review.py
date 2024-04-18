16fe7db93bf13574329a76bba21e73f3c5747257
Chiamablessing <chiamablessing@gmail.com>
2024-04-18 04:02:11 +0100

    Fix pycodestyle

--- models/review.py
+++ models/review.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """This is the review class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
@@ -17,4 +16,3 @@ class Review(BaseModel, Base):
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     text = Column(String(1024), nullable=False)
-    
